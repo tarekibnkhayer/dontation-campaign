b99873da709fd8a0412aac1c68f7a6814c07d567
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-09-26 22:18:08 +0600

    take search text and find category array successfully (not completed this ui

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 
 const Banner = () => {
- 
+    const [searchText, setSearchText] = useState();
+    const [data, setData] = useState([]);
+    useEffect(() =>{
+        fetch("donation.json")
+        .then(res => res.json())
+        .then(data => setData(data.data))
+    },[]);
+    const handleSearch = () => {
+        const searchedDatum = data.filter(datum => searchText.includes(datum.category));
+        console.log(searchedDatum);
+    };
+    const handleSearchText = e => {
+        setSearchText(e.target.value);
+    }
+   
+  
     return (
         <div  className="mt-4" >
             <div className=" w-full h-[500px] bg-[#FFFFFFF2] bg-texture bg-cover bg-center bg-blend-color-burn">
@@ -8,8 +24,8 @@ const Banner = () => {
                 <div className="relative top-44 text-center">
                     <h2 className="text-5xl font-bold">I Grow By Helping People In Need</h2>
                     <div className="mt-10">
-                        <input type="text" className="border w-96 h-12 p-4" placeholder="Search here..." />
-                        <button className="btn bg-[#FF444A] text-white hover:bg-red-800">Search</button>
+                        <input type="text" className="border w-96 h-12 p-4" placeholder="Search here..." onChange={handleSearchText} />
+                        <button className="btn bg-[#FF444A] text-white hover:bg-red-800" onClick={() => handleSearch()}>Search</button>
                     </div>
                 </div>
             </div>
