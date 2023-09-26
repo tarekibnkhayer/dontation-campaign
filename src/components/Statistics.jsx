cdcac32780daab0d7d29d5a58539dc99ec0b0082
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-09-26 20:44:22 +0600

    pie chart is successfully created

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,10 +1,35 @@
-
-
+import { useLoaderData } from 'react-router-dom';
+import { getStoredDonatedCards } from '../utility/LocalStorage';
+import React from 'react';
+import Chart from 'react-apexcharts'
 const Statistics = () => {
+    const totalDonations = useLoaderData();
+    const yourDonations = getStoredDonatedCards();
+    const yourDonationsLength = yourDonations.length
+    const totalDonationsLength = totalDonations.data.length - yourDonationsLength;
+    console.log(yourDonationsLength, totalDonationsLength);
+
     return (
+    <React.Fragment>
         <div>
-            <h2>This is Statistics</h2>
+            <Chart
+            type='pie'
+            width={1300}
+            height={550}
+            series={[totalDonationsLength, yourDonationsLength]} 
+            options={
+                {
+                noData: {text: "Empty data"},
+                labels: ['Total Donation', 'Your Donation'],
+                legend: {position: 'bottom'}
+                
+            }
+        }
+            >
+
+            </Chart>
         </div>
+    </React.Fragment>
     );
 };
 
