f792b04cea7da67ab492501398cb8b89d3977068
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-09-27 09:31:28 +0600

    ErrorPage Button Navigate is successfull

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,18 @@
+import { useNavigate, useRouteError,  } from "react-router-dom";
 
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1);
+    };
+    const error = useRouteError();
     return (
-        <div>
-            <h2>OOOOps!!!</h2>
-            <h3>Sorry We do not find it</h3>
-            <button className="btn btn-secondary">Go Back</button>
+        <div className="flex justify-center items-center flex-col mt-14  gap-6">
+            <h2 className="text-4xl">OOOOps!!!</h2>
+            <p className="text-3xl">Error : {error.statusText || error.message} </p>
+            <h3 className="text-3xl">Something Went Wrong</h3>
+            <button className="btn btn-secondary" onClick={() => handleGoBack()}>Go Back</button>
         </div>
     );
 };
